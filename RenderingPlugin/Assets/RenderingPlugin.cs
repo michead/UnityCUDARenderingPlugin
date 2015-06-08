718fda34d0a8ba031ea03d5109f88485a47823a1
michead <countrymetal@live.it>
2015-06-08 05:36:03 +0200

    memory allocated and released once on device

--- RenderingPlugin/Assets/RenderingPlugin.cs
+++ RenderingPlugin/Assets/RenderingPlugin.cs
@@ -6,27 +6,24 @@ using System;
 
 public class RenderingPlugin : MonoBehaviour
 {
-    public static Material lineMaterial;
     public static Mesh mesh;
 
     public static float FREQ = 4.0f;
-    public static int SIDE_SIZE = 250;
-    public static float SPACING = 0.25f; // totally useless -- DEPRECATED
-
-    struct float3
-    {
-        public float x, y, z;
-    }
+    public static uint SIDE_SIZE = 250;
 
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate void MyDelegate(string str);
 
     [DllImport("RenderingPluginDLL")]
-    public static extern void SetDebugFunction(IntPtr fp);
+    private static extern void Init([In, Out] Vector3[] verts, uint sideSize);
     [DllImport("RenderingPluginDLL")]
-    private static extern void ComputeSineWave([In, Out] float3[] verts, int sideSize, float time);
+    private static extern void Cleanup();
     [DllImport("RenderingPluginDLL")]
-    private static extern void ParallelComputeSineWave([In, Out] float3[] verts, int sideSize, float time);
+    private static extern void SetDebugFunction(IntPtr fp);
+    [DllImport("RenderingPluginDLL")]
+    private static extern void ComputeSineWave([In, Out] Vector3[] verts, float time);
+    [DllImport("RenderingPluginDLL")]
+    private static extern void ParallelComputeSineWave([In, Out] Vector3[] verts, float time);
 
     public string meshURL;
     Vector3[] verts;
@@ -34,11 +31,6 @@ public class RenderingPlugin : MonoBehaviour
 
     int numVerts;
     int numFaces;
-
-    void OnPostRender()
-    {
-
-    }
 
     void Start()
     {
@@ -56,112 +48,39 @@ public class RenderingPlugin : MonoBehaviour
         MyDelegate callbackDelegate = new MyDelegate(callback);
         IntPtr intptrDelegate = Marshal.GetFunctionPointerForDelegate(callbackDelegate);
         SetDebugFunction(intptrDelegate);
+
+        Init(verts, SIDE_SIZE);
     }
 
     void Update()
     {
         // computeSineWave();
-        parallelComputeSineWave(SIDE_SIZE, Time.timeSinceLevelLoad);
+        parallelComputeSineWave();
     }
 
-    # region DEPRECATED
-    void generatePlaneMesh()
+    void OnApplicationQuit()
     {
-        generateVerts();
-        generateTriangles();
-
-        mesh.vertices = verts;
-        mesh.triangles = triangles;
-    }
-
-    void generateVerts()
-    {
-        int index = 0;
-
-        for (int i = -SIDE_SIZE / 2; i < SIDE_SIZE / 2; i++) 
-        {
-            for (int j = -SIDE_SIZE / 2; j < SIDE_SIZE / 2; j++)
-            {
-                verts[index] = new Vector3( i * SPACING, 0, j * SPACING );
-
-                index++;
-            }
-        }
-    }
-
-    void generateTriangles()
-    {
-        int triangleCount = 0;
-
-        for (int i = 0; i < verts.Length; i++)
-        {
-            if ((i + 1) % SIDE_SIZE != 0)
-            {
-                if (i + SIDE_SIZE < verts.Length)
-                {
-                    triangles[triangleCount++] = i + 1;
-                    triangles[triangleCount++] = i + SIDE_SIZE;
-                    triangles[triangleCount++] = i;
-                }
-                if (i - SIDE_SIZE > -1)
-                {
-                    triangles[triangleCount++] = i + 1;
-                    triangles[triangleCount++] = i;
-                    triangles[triangleCount++] = i - SIDE_SIZE + 1;
-                }
-            }
-        }
+        Cleanup();
     }
 
     void computeSineWave()
     {
+        float elpasedTime = Time.timeSinceLevelLoad;
+
         for (int i = 0; i < verts.Length; i++) 
-        {
-            float elpasedTime = Time.timeSinceLevelLoad;
             verts[i].y = (float)(Math.Sin(verts[i].x * FREQ + elpasedTime) * Math.Cos(verts[i].z * FREQ + elpasedTime)) * 0.2f;
-        }
 
         mesh.vertices = verts;
         mesh.RecalculateNormals();
         GetComponent<MeshFilter>().mesh = mesh;
     }
-    #endregion
 
-    void parallelComputeSineWave(int sideSize, float time) 
+    void parallelComputeSineWave() 
     {
-        float3[] vertices = convertToFloat3(mesh.vertices);
-        ParallelComputeSineWave(vertices, sideSize, time);
-        mesh.vertices = convertToVector3(vertices);
+        ParallelComputeSineWave(verts, Time.timeSinceLevelLoad);
+        mesh.vertices = verts;
         mesh.RecalculateNormals();
         GetComponent<MeshFilter>().mesh = mesh;
-
-        /*
-        GCHandle handle = GCHandle.Alloc(convertToFloat(verts), GCHandleType.Pinned);
-        try
-        {
-            IntPtr pointer = handle.AddrOfPinnedObject();
-            // ParallelComputeSineWave(ref pointer, sideSize, time);
-            ComputeSineWave(ref pointer, sideSize, time);
-            
-            byte[] data = new byte[numVerts * sizeof(float)];
-            float[] fa = new float[numVerts * 3];
-
-            Marshal.Copy(pointer
-             , data
-             , 0
-             , data.Length);
-            Buffer.BlockCopy(data, 0, fa, 0, data.Length);
-
-            verts = convertToVector3(fa);
-        }
-        finally
-        {
-            if (handle.IsAllocated)
-            {
-                handle.Free();
-            }
-        }
-        */
     }
 
     void getMeshDataFromFile(string url)
@@ -229,55 +148,6 @@ public class RenderingPlugin : MonoBehaviour
                 triangles[index++] = int.Parse(tokens[3]) - 1;
             }
         }
-    }
-
-    float[] convertToFloat(Vector3[] verts)
-    {
-        float[] result = new float[verts.Length * 3];
-        int index = 0;
-
-        for (int i = 0; i < verts.Length; i++)
-        {
-            result[index++] = verts[i].x;
-            result[index++] = verts[i].y;
-            result[index++] = verts[i].z;
-        }
-
-        return result;
-    }
-
-    float3[] convertToFloat3(Vector3[] verts) 
-    {
-        float3[] result = new float3[verts.Length];
-        for (int i = 0; i < verts.Length; i++)
-        {
-            Vector3 vec = verts[i];
-            result[i] = new float3{ x = vec.x, y = vec.y, z = vec.z};
-        }
-
-        return result;
-    }
-
-    Vector3[] convertToVector3(float[] verts)
-    {
-        Vector3[] result = new Vector3[verts.Length / 3];
-
-        for (int i = 0; i < verts.Length; i += 3)
-            result[i / 3] = new Vector3(verts[i], verts[i + 1], verts[i + 2]);
-
-        return result;
-    }
-
-    Vector3[] convertToVector3(float3[] verts)
-    {
-        Vector3[] result = new Vector3[verts.Length];
-        for (int i = 0; i < verts.Length; i++)
-        {
-            float3 vec = verts[i];
-            result[i] = new Vector3(vec.x, vec.y, vec.z );
-        }
-
-        return result;
     }
 
     static void callback(string str)
--- RenderingPluginDLL/RenderingPluginDLL/kernel.cu
+++ RenderingPluginDLL/RenderingPluginDLL/kernel.cu
@@ -22,28 +22,25 @@
 #define NOMINAX
 #define EXPORT_API __declspec(dllexport)
 
-static float g_Time;
-static float g_Freq = 4.0f;
-
-static float mesh_width;
-static float mesh_height;
-
-static GLuint vbo;
-struct cudaGraphicsResource *cuda_vbo_resource;
-void *d_vbo_buffer = NULL;
+#define FREQ 4.0f
+#define BLOCK_DIM_X 10
+#define BLOCK_DIM_Y 10
 
 typedef void(*FuncPtr)(const char *);
 FuncPtr Debug;
+
+float3* devVerts;
+unsigned int sideSize;
 
 extern "C" EXPORT_API void SetDebugFunction(FuncPtr fp)
 {
 	Debug = fp;
 }
 
-extern "C" EXPORT_API void ComputeSineWave(float3* verts, int sideSize, float time)
+extern "C" EXPORT_API void ComputeSineWave(float3* verts, float time)
 {
 	for (int i = 0; i < sideSize * sideSize; i ++) 
-		verts[i].y = sin(verts[i].x * g_Freq + time) * cos(verts[i].z * g_Freq + time) * 0.2f;
+		verts[i].y = sin(verts[i].x * FREQ + time) * cos(verts[i].z * FREQ + time) * 0.2f;
 }
 
 __global__ void simple_vbo_kernel(float3 *pos, unsigned int sideSize, float time)
@@ -51,110 +48,30 @@ __global__ void simple_vbo_kernel(float3 *pos, unsigned int sideSize, float time
 	unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
 	unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
 
-	// calculate uv coordinates
-	// float u = x / (float)width;
-	// float v = y / (float)height;
-	// u = u*2.0f - 1.0f;
-	// v = v*2.0f - 1.0f;
-
-	// calculate simple sine wave pattern
 	float freq = 4.0f;
-	// float w = sinf(u*freq + time) * cosf(v*freq + time) * 0.5f;
-
-	// write output vertex
 	int index = y * sideSize + x;
+
 	pos[index].y = sinf(pos[index].x * freq + time) * cosf(pos[index].z * freq + time) * 0.2f;
 }
 
-extern "C" EXPORT_API void ParallelComputeSineWave(float3* verts, int sideSize, float time)
+extern "C" EXPORT_API void ParallelComputeSineWave(float3* verts, float time)
 {
-	float3* dev_verts;
-
-	cudaMalloc((void**)&dev_verts, sideSize * sideSize * sizeof(float3));
-	cudaMemcpy(dev_verts, verts, sideSize * sideSize * sizeof(float3), cudaMemcpyHostToDevice);
-
-	dim3 block(10, 10, 1);
+	dim3 block(BLOCK_DIM_X, BLOCK_DIM_Y, 1);
 	dim3 grid(sideSize / block.x, sideSize / block.y, 1);
-	simple_vbo_kernel << < grid, block >> >(dev_verts, sideSize, time);
+	simple_vbo_kernel << < grid, block >> >(devVerts, sideSize, time);
 
-	cudaMemcpy(verts, dev_verts, sideSize * sideSize * sizeof(float3), cudaMemcpyDeviceToHost);
-	cudaFree(dev_verts);
+	cudaMemcpy(verts, devVerts, sideSize * sideSize * sizeof(float3), cudaMemcpyDeviceToHost);
 }
 
-
-void launch_kernel(float3 *pos, unsigned int mesh_width, unsigned int mesh_height, float time)
+extern "C" EXPORT_API void Init(float3* verts, unsigned int size)
 {
-	// execute the kernel
-	// dim3 block(8, 8, 1);
-	// dim3 grid(mesh_width / block.x, mesh_height / block.y, 1);
-	// simple_vbo_kernel << < grid, block >> >(pos, mesh_width, mesh_height, time);
+	sideSize = size;
+
+	cudaMalloc((void**)&devVerts, sideSize * sideSize * sizeof(float3));
+	cudaMemcpy(devVerts, verts, sideSize * sideSize * sizeof(float3), cudaMemcpyHostToDevice);
 }
 
-
-void runCuda(struct cudaGraphicsResource **vbo_resource)
+extern "C" EXPORT_API void Cleanup()
 {
-	// map OpenGL buffer object for writing from CUDA
-	float3 *dptr;
-	checkCudaErrors(cudaGraphicsMapResources(1, vbo_resource, 0));
-	size_t num_bytes;
-	checkCudaErrors(cudaGraphicsResourceGetMappedPointer((void **)&dptr, &num_bytes,
-		*vbo_resource));
-	//printf("CUDA mapped VBO: May access %ld bytes\n", num_bytes);
-
-	// execute the kernel
-	//    dim3 block(8, 8, 1);
-	//    dim3 grid(mesh_width / block.x, mesh_height / block.y, 1);
-	//    kernel<<< grid, block>>>(dptr, mesh_width, mesh_height, g_fAnim);
-
-	launch_kernel(dptr, mesh_width, mesh_height, g_Time);
-
-	// unmap buffer object
-	checkCudaErrors(cudaGraphicsUnmapResources(1, vbo_resource, 0));
-}
-
-void createVBO(GLuint *vbo, struct cudaGraphicsResource **vbo_res, unsigned int vbo_res_flags)
-{
-	assert(vbo);
-
-	// TODO this GL calls need to be moved to the C# script
-
-	// create buffer object
-	// glGenBuffers(1, vbo);
-	// glBindBuffer(GL_ARRAY_BUFFER, *vbo);
-
-	// initialize buffer object
-	unsigned int size = mesh_width * mesh_height * 3 * sizeof(float);
-	// glBufferData(GL_ARRAY_BUFFER, size, 0, GL_DYNAMIC_DRAW);
-
-	// glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	// register this buffer object with CUDA
-	checkCudaErrors(cudaGraphicsGLRegisterBuffer(vbo_res, *vbo, vbo_res_flags));
-}
-
-void deleteVBO(GLuint *vbo, struct cudaGraphicsResource *vbo_res)
-{
-
-	// unregister this buffer object with CUDA
-	checkCudaErrors(cudaGraphicsUnregisterResource(vbo_res));
-
-	// glBindBuffer(1, *vbo);
-	// glDeleteBuffers(1, vbo);
-
-	*vbo = 0;
-}
-
-void cleanup()
-{
-	if (vbo)
-	{
-		deleteVBO(&vbo, cuda_vbo_resource);
-	}
-
-	// cudaDeviceReset causes the driver to clean up all state. While
-	// not mandatory in normal operation, it is good practice.  It is also
-	// needed to ensure correct operation when the application is being
-	// profiled. Calling cudaDeviceReset causes all profile data to be
-	// flushed before the application exits
-	cudaDeviceReset();
+	cudaFree(devVerts);
 }
